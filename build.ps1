f93bc32d4c36adb9b92052aae3dd72c452010ad9
mass <mass@steamknight.com>
2022-07-19 13:14:42 -0700

    Getting closer to finishing dialogues
    Now you can have a whole dialogue!

--- build.ps1
+++ build.ps1
@@ -11,10 +11,10 @@ $bb2_files = @(
     "./src/SC_scene.bb2"
     "./src/SH_shapes.bb2"
     "./src/IV_inventory.bb2"
-    "./src/DG_dialogue.bb2",
     "./src/MS_music.bb2",
     "./src/NS_opcodes.bb2",
     "./src/NS_script.bb2",
+    "./src/DG_dialogue.bb2",
     "./src/NN_main.bb2"
     #"./src/location_tester.bb2"
 )
